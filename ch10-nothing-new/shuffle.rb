156789e868b021771da93c804e12a1f314899a22
deonjtan@gmail.com <deonjtan@gmail.com>
2015-10-05 04:08:41 +0100

    Finished shuffle. One failure -"maintains all the same elements"

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,14 @@
-def shuffle arr
+def shuffle array
   # your code here
+  recursive_shuffle array, []
+end
+
+def recursive_shuffle array, shuffled
+  if array.length == 0
+    return shuffled
+  end
+  rnd_i = rand(array.length)
+  shuffled << array[rnd_i]
+  array.delete_at(rnd_i)
+  recursive_shuffle array, shuffled
 end
