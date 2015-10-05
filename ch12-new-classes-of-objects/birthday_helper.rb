390fdeb249c4927d75eedf04cea0e2b7c8230596
deonjtan@gmail.com <deonjtan@gmail.com>
2015-10-05 22:25:09 +0100

    filled in birthday_helper

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,18 @@
-# your code here
+birth_dates = {}
+File.read('birthdates.txt').each_line do |line|
+  line = line.chomp
+  first_comma = line.index(',')
+  name = line[0..(first_comma - 1)]
+  date = line[-12..-1]
+  birth_dates[name] = date
+end
+
+puts 'Please enter a name?'
+name = gets.chomp
+date = birth_dates[name]
+
+if date == nil
+  puts 'Entry does not exist'
+else
+  puts date[0..5]
+end
