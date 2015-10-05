786d6a47d95d6976aded46cb9a175b2c1c09a9ec
deonjtan@gmail.com <deonjtan@gmail.com>
2015-10-05 05:56:54 +0100

    dictionary_sort completed. Test passed

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,23 @@
-def dictionary_sort arr
-  # your code here
+def dictionary_sort array
+  d_rec_sort array, []
 end
+
+def d_rec_sort unsorted, sorted
+  if unsorted == []
+    return sorted
+  end
+  clone_array = []
+  min_e = unsorted.pop
+  unsorted.each {|cf_e| 
+    if min_e.downcase < cf_e.downcase
+      clone_array.push cf_e
+    else
+      clone_array.push min_e
+      min_e = cf_e
+    end
+    }
+  sorted.push(min_e)
+  d_rec_sort clone_array, sorted
+end
+
+dictionary_sort ["Banana","banana","BAnANAna","Apple","apple","cherry","zebra","elephand"]
