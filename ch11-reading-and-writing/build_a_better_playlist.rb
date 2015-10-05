a5e3bfcdca92e6aa5df4744bcbc74937d4254004
deonjtan@gmail.com <deonjtan@gmail.com>
2015-10-05 12:06:34 +0100

    added changes based on CP solutions, still cannot get rspec test to pass

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -2,10 +2,10 @@ def music_shuffle filenames
   music_files = filenames #music_shuffle(Dir['**/*.{mp3,MP3,wav,WAV,flac,FLAC}'])
   music_files.sort
   pivot = music_files.length / 2
-  3.times do
-    odds_arr = []
-    evns_arr = []
-    comb_arr = []
+  odds_arr = []
+  evns_arr = []
+  comb_arr = []
+  3.times {
     for index in (0...pivot)
       odds_arr << music_files[index]
     end
@@ -19,22 +19,17 @@ def music_shuffle filenames
     m = evns_arr.length
     m.times {|index| comb_arr[(index*2)+1] = evns_arr[index]}
     comb_arr.compact!
-  return comb_arr
+    comb_arr
+  }
+  len = music_files.length
+  arr = []
+  cut = rand(len) # index of card to cut at
+  idx = 0
+
+  while idx < len
+    arr.push(comb_arr[(idx+cut)%len])
+    idx = idx + 1
   end
+
+  arr
 end
-music_shuffle [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-
-## create playlist
-Dir.chdir '/home/deon/Music'
-timestamp = Time.new
-timestamp = timestamp.to_s
-filename = "playlist - #{timestamp}.m3u"
-
-
-music_files = music_shuffle(Dir['**/*.{mp3,MP3,wav,WAV,flac,FLAC}'])
-
-File.open filename,'w' do |f| music_files.each do |track| f.write track+"\n"
-  end
-end
-
-puts "djtango"
