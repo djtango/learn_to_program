ff183e8eab31741ace15a414d61dff77a5db5dbb
deonjtan@gmail.com <deonjtan@gmail.com>
2015-10-05 08:26:53 +0100

    Added basic playlist code

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,19 @@
 # your code here
+## create playlist
+
+Dir.chdir '/home/deon/Music'
+timestamp = time.new
+timestamp.to_s!
+filename = "playlist - #{timestamp}.m3u"
+
+File.open filename 'w' #{}
+
+music_files = shuffle(Dir['!/**/*.{jpg,JPG,WAV,flac,FLAC,wav}'])
+
+
+
+File.open 'playlist','w' { |f| music_files.each { |track| f.write track+"\n"
+  }
+}
+
+puts "djtango"
