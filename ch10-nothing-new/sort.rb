28c87aaee795183e4aed9739ff71a12e07d2ac7d
deonjtan@gmail.com <deonjtan@gmail.com>
2015-10-05 01:38:04 +0100

    sort complete - test passed

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,20 @@
-def sort arr
+def sort array
   # your code here
+  recursive_sort array, []
+end
+
+def sort array
+  # your code here
+  recursive_sort array, []
+end
+
+def recursive_sort unsorted, sorted
+  unsorted.compact!
+  if unsorted == []
+    return sorted
+  else
+    sorted << unsorted.min
+    unsorted.delete_at(unsorted.index(unsorted.min))
+  end
+  recursive_sort unsorted, sorted
 end
