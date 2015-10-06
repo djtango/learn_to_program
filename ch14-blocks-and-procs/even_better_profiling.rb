888d66591c608236571d29aca41d0073c713dd92
deonjtan@gmail.com <deonjtan@gmail.com>
2015-10-06 20:16:00 +0100

    Code added

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,14 @@
 def profile block_description, &block
-  # your code here
+  $profile_on == true
+  if $profile_on
+    start_time = Time.new
+    block.call
+    duration = Time.new - start_time
+    puts "#{block_description}: #{duration} seconds"  # your code here
+    $profile_on = !$profile_on
+  else
+    block.call
+  end
 end
+
+profile_on
