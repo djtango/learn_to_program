cdf5dc5ff7d71d1f0931ce14b7288c5e905db6fe
deonjtan@gmail.com <deonjtan@gmail.com>
2015-10-06 21:20:48 +0100

    CP completed!

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,9 @@
+$nesting_depth = 0
 def log desc, &block
-  # your code here
+  $nesting_depth += 1
+  ind = '  ' * ($nesting_depth-1)
+  puts "#{ind}#{desc} commencing"
+  returned = block[]*
+  puts "#{ind}#{desc} finished, returning:"
+  puts "#{ind}#{returned}"  # your code here
 end
